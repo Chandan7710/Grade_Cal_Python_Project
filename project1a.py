d4618a987eb1e2e283861c7209912c629243ea0d
chandan7710 <chandan.ha@quadwave.com>
2022-12-08 16:25:10 +0530

    Grade Cal Python Project

---
+++ project1a.py
@@ -0,0 +1,33 @@
+
+# PROJECT - 1 ==> Grade Calculator
+
+'''
+Finding Student Grade and Percentage using the score
+and displaying the student Name, Grade and Percentage
+'''
+
+maxscore = 900
+score = 700
+studentname = "Chandan H A"
+percentage = round((score/maxscore)*100, 3)
+
+'''
+A = 100 - 90 %
+B = 89 - 80 %
+C = 79 - 70 %
+D = 69 - 60 %
+E = 59 - 0 %
+'''
+
+# Print the student name and what the grade they got and percentage scored 
+
+if 100 >= percentage >= 90:
+    print(f"{studentname} has got the Grade-A and Percentage = {percentage}")
+elif 89 >= percentage >= 80:
+    print(f"{studentname} has got the Grade-B and Percentage = {percentage}")
+elif 79 >= percentage >= 70:
+    print(f"{studentname} has got the Grade-C and Percentage = {percentage}")
+elif 69 >= percentage >= 60:
+    print(f"{studentname} has got the Grade-D and Percentage = {percentage}")
+else:
+    print(f"{studentname} has got the Grade-E and Percentage = {percentage}")
